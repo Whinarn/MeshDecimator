d8bdd002e0bff04d8e7b94a08e91ba50f2369aa0
Whinarn <matte.edlund@gmail.com>
2017-12-07 00:52:17 +0900

    Created a script for the example scene in the Unity Example project.

---
+++ UnityExample/Assets/Scripts/ShowExample.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class ShowExample : MonoBehaviour
+{
+    [SerializeField]
+    private Transform cameraTransform = null;
+    [SerializeField]
+    private Transform targetTransform = null;
+
+    [SerializeField]
+    private float cameraAngleTime = 1f;
+    [SerializeField]
+    private float cameraMinDistance = 3f;
+    [SerializeField]
+    private float cameraMaxDistance = 30f;
+    [SerializeField]
+    private float cameraDistanceTime = 1f;
+    [SerializeField]
+    private float cameraSwayHeight = 2f;
+    [SerializeField]
+    private float cameraSwayTime = 1f;
+
+    private void Update()
+    {
+        float angle = Mathf.Repeat(Time.time / cameraAngleTime, Mathf.PI * 2f);
+
+        float distanceT = Mathf.Max(Mathf.Sin(Time.time / cameraDistanceTime), 0f);
+        float distance = Mathf.Lerp(cameraMinDistance, cameraMaxDistance, distanceT);
+
+        float altitude = Mathf.Cos(Time.time / cameraSwayTime) * cameraSwayHeight * distance;
+
+        cameraTransform.position = targetTransform.position +
+            new Vector3(Mathf.Cos(angle) * distance, altitude, Mathf.Sin(angle) * distance);
+
+        cameraTransform.rotation = Quaternion.LookRotation(targetTransform.position - cameraTransform.position);
+    }
+}
